7ba53ef45bf0f4bf9b985d0ab41f0b00767cefba
rchaser53 <tayoshizawa29@gmail.com>
2022-10-29 22:11:54 +0900

    add reference-type sample

---
+++ src/reference_type.rs
@@ -0,0 +1,16 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let val = document.create_element("p")?;
+    val.set_text_content(Some("Hello from Rust!"));
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
